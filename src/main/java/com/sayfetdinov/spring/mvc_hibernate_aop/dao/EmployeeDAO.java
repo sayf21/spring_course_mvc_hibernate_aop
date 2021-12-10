deab1cc2295a780cce42be708c0626e884222549
Ilya Sayfetdinov <sayfetdinov1408@yandex.ru>
2021-12-10 16:43:46 +0500

    Initial commit

---
+++ src/main/java/com/sayfetdinov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.sayfetdinov.spring.mvc_hibernate_aop.dao;
+
+import com.sayfetdinov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
